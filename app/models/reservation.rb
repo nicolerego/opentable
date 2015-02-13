93c875dfe049fa94a6a6a08a41712211dc65cd60
JRumble <jody.paget@rogers.com>
2015-02-13 14:39:35 -0500

    updating to master file

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,14 +2,14 @@ class Reservation < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
 
-	# validates :party_size, :party_time, presence: true
+	validates :party_size, :party_time, presence: true
 
-	# validate :availability
+	validate :availability
 
-	# private
-	# def availability
-	# 	if !restaurant.available?(party_size, party_time)
-	# 		errors.add(:base, "Unfortunately, no reservations are available for this time.")
-	# 	end
-	# end
+	private
+	def availability
+		if !restaurant.available?(party_size, party_time)
+			errors.add(:base, "Unfortunately, no reservations are available for this time.")
+		end
+	end
 end
