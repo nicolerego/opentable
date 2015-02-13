a75d0edffd5f6c0934c103a51c9edb945998c4b7
JRumble <jody.paget@rogers.com>
2015-02-13 15:40:19 -0500

    Added funky seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def random_hour(from, to)
+  (Date.today + rand(from..to).hour + rand(0..60).minutes).to_datetime
+end
+
+puts "\nCreating users..."
+100.times do
+	User.create!(
+		name: 		Faker::Name.name,
+		email: 		Faker::Internet.email,
+		password: 	"asdf",
+		user_group: ["owner", "dinner"].sample
+	)
+	print "|"
+end
+
+puts "\nCreating restaurants..."
+70.times do
+	Restaurant.create!(
+		name: 			Faker::Company.name, 
+		location: 		Faker::Address.street_address,
+		phone_number: 	Faker::PhoneNumber.cell_phone,
+		open_hours: 	["9-5", "10-10", "12-4"].sample,
+		description: 	Faker::Company.catch_phrase,
+		menu: 			Faker::Lorem.paragraph(2),
+		price_range: 	["10-20", "20-40", "40-60", "60-100"].sample,
+		capacity: 		[20, 40, 60, 100].sample,
+		picture: 		Faker::Company.logo
+	)
+	print "|"
+end
+
+puts "\nCreating reservations..."
+500.times do
+	Reservation.create!(
+		user_id: 		User.all.sample.id, 
+		restaurant_id: 	Restaurant.all.sample.id,
+		date: 			rand(10).days.from_now.to_date,
+		time: 			random_hour(10, 18),
+		party_size: 	[2,3,4,6,10,20].sample
+	)
+	print "|"
+end
