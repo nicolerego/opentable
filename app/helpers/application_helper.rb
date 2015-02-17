45e644beec8e7db4c39427b49229e97fe1a06576
Nicole Rego <nicole.rego@live.com>
2015-02-17 12:03:24 -0500

    changing reservation_time_select and restaurant_time_select methods to hold hour, editing views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 	# Create and array to use to :time f.select 
 	def reservation_time_select(restaurant)
-		availableTimes = (restaurant.open_time...restaurant.close_time).map { |hour| formatted_time(hour) } 
+		availableTimes = (@restaurant.open_time...@restaurant.close_time).map { |hour| [formatted_time(hour), hour] } 
 	end
 
-	# To get range to work (on 24 hour clock)
+	# To get range to work 
 	def restaurant_time_select
-		times = (0..23).map { |hour| formatted_time(hour) }
+		times = (0..23).map { |hour| [formatted_time(hour), hour] }
 	end
 
 	def formatted_time(hour)
